92ca980944aa3ed866a41b4e951ae89eedef9d63
slemanz <william_sleman@hotmail.com>
2026-07-13 19:21:10 -0300

    thread(refact): pass led pin by pointer parameter

--- thread/app/Src/main.c
+++ thread/app/Src/main.c
@@ -5,36 +5,52 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void vBlueLedControllerTask(void *pvParameters);
-void vRedLedControllerTask(void *pvParameters);
-void vGreenLedControllerTask(void *pvParameters);
+#define LED_RED             GPIO_PIN_NO_3
+#define LED_YELLOW          GPIO_PIN_NO_4
+#define LED_GREEN           GPIO_PIN_NO_5
 
-typedef uint32_t TaskProfiler;
+void vLedControllerTask(void *pvParameters);
 
-TaskProfiler BlueTaskProfiler, RedTaskProfiler, GreenTaskProfiler;
+static const uint8_t red_led    = LED_RED;
+static const uint8_t yellow_led = LED_YELLOW;
+static const uint8_t green_led  = LED_GREEN;
+
+static void led_init(GPIO_RegDef_t *pGPIOx, uint8_t pin)
+{
+    GPIO_PinConfig_t led;
+    led.pGPIOx = pGPIOx;
+    led.GPIO_PinNumber = pin;
+    led.GPIO_PinMode = GPIO_MODE_OUT;
+    led.GPIO_PinSpeed = GPIO_SPEED_FAST;
+    led.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+    led.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+    led.GPIO_PinAltFunMode = GPIO_PIN_NO_ALTFN;
+    GPIO_Init(&led);
+}
+
 
 int main(void)
 {
     config_app();
 
-    xTaskCreate(vBlueLedControllerTask,
-                "Blue Led Controller",
-                100,
-                NULL,
-                1,
-                NULL);
-
-    xTaskCreate(vGreenLedControllerTask,
-                "Green Led Controller",
-                100,
-                NULL,
-                1,
-                NULL);
-
-    xTaskCreate(vRedLedControllerTask,
+    xTaskCreate(vLedControllerTask,
                 "Red Led Controller",
                 100,
-                NULL,
+                (void *)&red_led,
+                1,
+                NULL);
+
+    xTaskCreate(vLedControllerTask,
+                "Yellow Led Controller",
+                100,
+                (void *)&yellow_led,
+                1,
+                NULL);
+
+    xTaskCreate(vLedControllerTask,
+                "Green Led Controller",
+                100,
+                (void *)&green_led,
                 1,
                 NULL);
 
@@ -45,29 +61,15 @@ int main(void)
     }
 }
 
-void vBlueLedControllerTask(void *pvParameters)
+void vLedControllerTask(void *pvParameters)
 {
-    while(1)
-    {
-        BlueTaskProfiler++;
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-}
+    uint8_t pin = *(const uint8_t *)pvParameters;
 
-void vRedLedControllerTask(void *pvParameters)
-{
-    while(1)
-    {
-        RedTaskProfiler++;
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-}
+    led_init(GPIOB, pin);
 
-void vGreenLedControllerTask(void *pvParameters)
-{
     while(1)
     {
-        GreenTaskProfiler++;
-        vTaskDelay(pdMS_TO_TICKS(500));
+        GPIO_ToggleOutputPin(GPIOB, pin);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
